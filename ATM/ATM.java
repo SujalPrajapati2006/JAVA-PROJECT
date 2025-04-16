fc50ff015d2ce226f2a7acf3c2a66cf3353fac40
SujalPrajapati2006 <prajapatisujal123456@gmail.com>
2025-04-16 14:07:47 +0530

    Add files via upload

---
+++ ATM/ATM.java
@@ -0,0 +1,10 @@
+package ATM;
+
+public class ATM extends OptionMenu {
+    public static void main(String[] args) {
+
+        OptionMenu option = new OptionMenu();
+        option.getLogin();
+
+    }
+}
